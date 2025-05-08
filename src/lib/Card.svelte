1c66515da93ce26c6d272e41e8c40e39445b3ba7
Shara88 <Shara88@student.op.ac.nz>
2025-05-08 16:23:35 +1200

    card

---
+++ src/lib/Card.svelte
@@ -0,0 +1,39 @@
+<script>
+let cardData = {
+    title: "My Card",
+    image: "https://picsum.photos/id/237/200/300",
+    info:"This is the card information.",
+};
+</script>
+
+<div>
+    <h2>
+        {cardData.title}
+    </h2>
+
+    <section class="card-info">
+        <img src={cardData.image} alt="Coming soon">
+        <p> {cardData.info} </p>
+        
+    </section>
+    
+</div>
+
+<style>
+div{
+    border: 1px solid grey;
+    max-width: 300px;
+    border-radius: 3px;
+    background-color: bisque;
+    box-shadow: 5px 5px grey;
+    margin: 0 1em 1em 0;
+    padding: 0%;
+    
+    
+}
+section{
+    display: flex;
+}
+
+</style>
+
---
+++ src/lib/UserDisplay.svelte
@@ -0,0 +1,4 @@
+<script>
+    let userName = "username";
+    let img = "https://picsum.photos/id/237/200/300";
+</script>

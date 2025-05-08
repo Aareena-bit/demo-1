95c45fad56bd64cdf40cb03f8bed98a0a431227d
Shara88 <Shara88@student.op.ac.nz>
2025-05-08 16:23:48 +1200

    p

---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+
+<script>
+
+    import Footer from "$lib/Footer.svelte";
+  import UserDisplay from "$lib/UserDisplay.svelte";
+    import Card from "$lib/Card.svelte";
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<Card />
+
+<Card />
+
+<UserDisplay />
+
+
+<Footer />

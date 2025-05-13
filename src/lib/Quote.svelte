05391c00bd0e16f2733fe303dfed2a765313e623
Aareena Sharma (1000125473) <SHARA88@student.op.ac.nz>
2025-05-13 15:54:08 +1200

    CHanges

---
+++ src/lib/Quote.svelte
@@ -0,0 +1,21 @@
+<script>
+let quote = {
+    quoteText: "Words can be like X-rays.",
+    author: "Aldous Huxley", 
+publication: "Brave new world",
+citeUrl: "https://www.huxley.net/bnw/four.html",
+}
+</script>
+
+
+<div>
+    <blockquote cite={quote.citeUrl}> 
+<p>
+{quote.quoteText}    
+</p>
+</blockquote>
+<p>-{quote.author}, <cite>{quote.publication}</cite></p>
+</div>
+<style>
+
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,11 +4,24 @@
     import Footer from "$lib/Footer.svelte";
   import UserDisplay from "$lib/UserDisplay.svelte";
     import Card from "$lib/Card.svelte";
+    import Quote from "$lib/Quote.svelte";
+
+    
+      let quote = {
+    quoteText: "Words can be like X-rays.",
+    author: "Aldous Huxley", 
+publication: "Brave new world",
+citeUrl: "https://www.huxley.net/bnw/four.html",
+    }
+
 </script>
+
+
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
+<Quote {quote}/>
 <Card />
 
 <Card />
